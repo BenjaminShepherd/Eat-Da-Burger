33a63cf19e6f36758123d07e38d7276b0b32ff10
BenShep <19ute14@gmail.com>
2019-05-14 16:30:07 -0600

    working connection to mysql

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //dependencies
 var express = require("express");
+var exphbs = require("express-handlebars")
 
 //create our PORT 
 var PORT = process.env.PORT || 8000;
@@ -9,6 +10,9 @@ app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }))
 
+var routes = require('../Eat-Da-Burger/controllers/burger')
+
+app.use(routes)
 
 app.listen(PORT, function () {
     console.log("Server is listening on PORT:" + PORT)
